0ec9d3e5ec8756fac515be300233bf81e746651a
Andrea Frusteri <andrea.frusteri@studio.unibo.it>
2026-04-23 10:59:44 +0200

    23/04

---
+++ C01A_Parameters.m
@@ -0,0 +1,34 @@
+%% DC MOTOR PARAMETER
+
+%clear;
+
+%% DC MOTOR PARAMETER
+
+L = 4.1e-3; 	% Inductance (H)
+R = 3.2;     	% Resistance (Ohm) 
+Km = 0.11;   	% Torque constant (Nm/A)
+J = 0.352e-4; 	% Rotor Inertia(kg m2)
+b =(Km*0.3)/((1750*(2*pi))/60);       	% Coefficient of friction
+
+
+Cn=0.22;        % Nominal Torque (Nm)
+
+%% CONVERSION
+RadToRpm=60/(2*pi); %Convert rad->RPM
+RpmToRad = 2*pi/60;
+
+%%OTHER
+Tsample=0.0001;
+
+
+%%Encoder
+PulseNumber = 2048*4;
+RadiantsPerCount=2*pi/(PulseNumber-1);
+
+%% Power Supply Voltage sense
+ADCResolution = 2^12;
+ADCCount=ADCResolution-1;
+VrefADC=3;
+GainVsense=4.99/(62+4.99); % Partitore al sensore ADCIN
+
+
